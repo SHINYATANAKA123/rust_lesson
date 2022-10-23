25b83f141737a0ae734e9accef5d08d83734bfdb
Shinya Tanaka <tanakashinya@team-lab.com>
2022-10-23 16:51:54 +0900

    所有権・参照・借用

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-mod stack_heap;
+// mod stack_heap;
 // mod vars;
+mod ownership;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
-    stack_heap::run();
+    // stack_heap::run();
+    ownership::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
